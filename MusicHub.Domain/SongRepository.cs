26fbc215c89b58e4ba899ac3bebccd8f2222c09f
zvonimir-delas <6d.solin@gmail.com>
2017-08-23 23:01:36 +0200

    Added method for adding a new song

--- MusicHub.Domain/SongRepository.cs
+++ MusicHub.Domain/SongRepository.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MusicHub.Data;
 
 namespace MusicHub.Domain
 {
-    class SongRepository
+    public class SongRepository
     {
+        public void AddNewSong(string name, string duration, Author author)
+        {
+            using (var context = new MusicHubDb())
+            {
+                context.Songs.Add(new Song()
+                {
+                    Id = context.Songs.ToList().Count(),
+                    Name = name,
+                    Duration = TimeSpan.Parse(duration),
+                    Author1 = author
+                });
+
+                context.SaveChanges();
+            }
+        }
     }
 }
