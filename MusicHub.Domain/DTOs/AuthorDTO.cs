926ed710c9b89703c10989daaf6b261c91ad3009
zvonimir-delas <6d.solin@gmail.com>
2017-08-23 16:10:38 +0200

    Added SongDTO and AuthorDTO

---
+++ MusicHub.Domain/DTOs/AuthorDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicHub.Data;
+
+namespace MusicHub.Domain.DTOs
+{
+    public class AuthorDTO
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public List<SongDTO> Songs { get; set; }
+
+        public static AuthorDTO FromEntity(Author author)
+        {
+            return new AuthorDTO
+            {
+                Id = author.Id,
+                FullName = author.FullName,
+                Songs = author.Songs.Select(x => SongDTO.FromEntity(x)).ToList()
+            };
+
+    }
+    }
+}
---
+++ MusicHub.Domain/DTOs/SongDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicHub.Data;
+
+namespace MusicHub.Domain.DTOs
+{
+    public class SongDTO
+    {
+        public int Id { get; set; }
+        public string NameAndDuration { get; set; }
+        
+        public static SongDTO FromEntity (Song song)
+        {
+            return new SongDTO
+            {
+                Id = song.Id,
+                NameAndDuration = $"{song.Name} {song.Duration.Value.Hours} hour(s) {song.Duration.Value.Minutes} minute(s) {song.Duration.Value.Seconds} second(s)"
+            };
+        }
+    }
+}
