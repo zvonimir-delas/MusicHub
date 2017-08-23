4f261b8f0dc6674bb37f8a1dfba0c842a2da8401
zvonimir-delas <6d.solin@gmail.com>
2017-08-23 22:34:28 +0200

    Added support for deleting and adding an Author and viewing his/her's songs

---
+++ MusicHub.Domain/AuthorRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MusicHub.Data;
+using MusicHub.Domain.DTOs;
+using System.Data.Entity;
+
+namespace MusicHub.Domain
+{
+    public class AuthorRepository
+    {
+        public AuthorDTO GetAuthorByName(string fullName)
+        {
+            using (var context = new MusicHubDb())
+            {
+                return context.Authors.Include("Songs").Where(x => x.FullName == fullName).ToList().Select(y => AuthorDTO.FromEntity(y)).FirstOrDefault();
+            }
+        }
+
+        public void AddAuthor(string fullName, string password)
+        {
+            using (var context = new MusicHubDb())
+            {
+                context.Authors.Add(new Author()
+                {
+                    Id = context.Authors.ToList().Count(),
+                    FullName = fullName,
+                    Password = password
+                });
+
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteAuthor(string fullName, string password)
+        {
+            using (var context = new MusicHubDb())
+            {
+                Author author = context.Authors.Where(x => x.FullName == fullName && x.Password == password).FirstOrDefault();
+                context.Entry(author).State = EntityState.Deleted;
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- MusicHub.Presentation/Program.cs
+++ MusicHub.Presentation/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MusicHub.Domain;
 
 namespace MusicHub.Presentation
 {
@@ -10,6 +11,59 @@ namespace MusicHub.Presentation
     {
         static void Main(string[] args)
         {
+            var authorRepository = new AuthorRepository();
+            while (true)
+            {
+                Console.WriteLine("Pick an option\n 1) Access Authors\n 2) Access Songs");
+
+                int optionChosen = int.Parse(Console.ReadLine());
+
+                if (optionChosen == 1)
+                {
+                    Console.WriteLine("Pick an option\n 1) View songs by an author\n 2) Add an author\n 3) Remove an author");
+
+                    optionChosen = int.Parse(Console.ReadLine());
+
+                    if (optionChosen == 1)
+                    {
+                        Console.WriteLine("Name: ");
+                        string name = Console.ReadLine();
+                        authorRepository.GetAuthorByName(name).Songs.ToList().ForEach(x => Console.WriteLine(x.NameAndDuration)); //TRY CATch
+                    }
+
+                    else if (optionChosen == 2)
+                    {
+                        string name; //does not work if name is defined inside 'do' block
+                        do
+                        {
+                            Console.WriteLine("Name: ");
+                            name = Console.ReadLine();
+                        }
+                        while (authorRepository.GetAuthorByName(name) != null);
+
+                        Console.WriteLine("Password: ");
+                        string password = Console.ReadLine();
+
+                        authorRepository.AddAuthor(name, password);
+                    }
+
+                    else if (optionChosen == 3)
+                    {
+                        string name; //does not work if name is defined inside 'do' block
+                        do
+                        {
+                            Console.WriteLine("Name: ");
+                            name = Console.ReadLine();
+                        }
+                        while (authorRepository.GetAuthorByName(name) == null);
+
+                        Console.WriteLine("Password: ");
+                        string password = Console.ReadLine();
+
+                        authorRepository.DeleteAuthor(name, password);
+                    }
+                }
+            }
         }
     }
 }
